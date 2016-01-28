7cc1c6119e0a08cbd96f6f6734ba50d76a99be88
terakilobyte <terakilobyte@gmail.com>
2016-01-28 05:38:45 -0800

    testing koan

--- about_testing.exs
+++ about_testing.exs
@@ -4,23 +4,23 @@
 Koans.About_testing.start_exUnit!
 
 defmodule About_testing do
-    use FakeUnit.Case
+    use ExUnit.Case
     use Koans.About_testing
 
     think "We shall contemplate truth by testing reality, via asserts." do
-        assert __?
+        assert true == true
     end
 
     think "When reality lies, we shall refute truth" do
-        refute __?
+        refute true == false
     end
 
     think "Enlightenment may be more easily achieved with appropriate messages." do
-        assert __?, "This should be true -- Please fix this"
+        assert true == true, "This should be true"
     end
 
     think "To understand reality, we must compare our expectations against reality." do
-        expected_value = __?
+        expected_value = 2
         actual_value = 1 + 1
 
         assert expected_value == actual_value
@@ -30,10 +30,10 @@ defmodule About_testing do
         is_1_equal_2? = fn -> assert 1 == 2 end
         is_1_greater_than_2? = fn -> assert 1 > 2 end
 
-        message = "Assertion with " <> __? <> " failed"
+        message = "Assertion with " <> "==" <> " failed"
         assert_raise ExUnit.AssertionError, message, is_1_equal_2?
 
-        message = "Assertion with " <> __? <> " failed"
+        message = "Assertion with " <> ">" <> " failed"
         assert_raise ExUnit.AssertionError, message, is_1_greater_than_2?
     end
 end
