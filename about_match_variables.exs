51bda1a915eedc977b76cdd89f93919c883c416c
terakilobyte <terakilobyte@gmail.com>
2016-01-29 05:07:32 -0800

    about_match_variables koans

--- about_match_variables.exs
+++ about_match_variables.exs
@@ -1,52 +1,52 @@
-#!/usr/bin/env elixir
+# #!/usr/bin/env elixir
 
-ExUnit.start
+# ExUnit.start
 
-defmodule About_Matches_Variables do
-  use ExUnit.Case
-  use Koans
+# defmodule About_Matches_Variables do
+#   use ExUnit.Case
+#   use Koans
 
-  think "It's like affecting a variable" do
-    a_variable = 1
-    assert a_variable == __?
-  end
+#   think "It's like affecting a variable" do
+#     a_variable = 1
+#     assert a_variable == 1
+#   end
 
-  think "The real operation is named matching" do
-    [a, b] = [1, 2]
+#   think "The real operation is named matching" do
+#     [a, b] = [1, 2]
 
-    assert a  == __? and b == __?
-  end
+#     assert a  == 1 and b == 2
+#   end
 
-  think "Matching three variables" do
-    [a, b, c] = [1, 2, 3]
+#   think "Matching three variables" do
+#     [a, b, c] = [1, 2, 3]
 
-    assert a  == __? and b == __? and c == __?
-  end
+#     assert a  == 1 and b == 2 and c == 3
+#   end
 
-  think "Matching one variable and drop a value" do
-    [a, _] = ["Keep me", "Drop me"]
+#   think "Matching one variable and drop a value" do
+#     [a, _] = ["Keep me", "Drop me"]
 
-    assert a  == __?
-  end
+#     assert a  == "Keep me"
+#   end
 
-  think "Error matching" do
-    assert_raise __?, fn -> [ a , a ] = [ 1 , 2 ] end
-  end
+#   think "Error matching" do
+#     assert_raise MatchError, fn -> [ a , a ] = [ 1 , 2 ] end
+#   end
 
-  think "Matching an array inner an array" do
-    [ a ] = [ ["Hello", "World !"] ]
+#   think "Matching an array inner an array" do
+#     [ a ] = [ ["Hello", "World !"] ]
 
-    assert a  == __?
-  end
+#     assert a  == ["Hello", "World !"]
+#   end
 
-  think "matching a tuple" do
-    { :ok, result } = { :ok, 12 }
-    assert result == __?
-  end
+#   think "matching a tuple" do
+#     { :ok, result } = { :ok, 12 }
+#     assert result == 12
+#   end
 
-  think "heads and tails" do
-    [ head | tail ] = [1, 2, 3]
-    assert head == __?
-    assert tail == __?
-  end
-end
+#   think "heads and tails" do
+#     [ head | tail ] = [1, 2, 3]
+#     assert head == 1
+#     assert tail == [2, 3]
+#   end
+# end
